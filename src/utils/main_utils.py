1ae1a81f3fb5214f8587b924dfd6387db149eb6d
Piyush chaubey <142138898+piyush230502@users.noreply.github.com>
2025-01-13 18:37:19 +0530

    Update main_utils.py

--- src/utils/main_utils.py
+++ src/utils/main_utils.py
@@ -75,3 +75,67 @@ class MainUtils:
 
         except Exception as e:
             raise CustomException(e, sys)
+
+             
+    @staticmethod
+    def download_model(bucket_name, bucket_file_name, dest_file_name):
+        try:
+            s3_client = boto3.client("s3")
+
+            s3_client.download_file(bucket_name, bucket_file_name, dest_file_name)
+
+            return dest_file_name
+
+        except Exception as e:
+            raise CustomException(e, sys)
+
+    @staticmethod
+    def remove_unwanted_spaces(data: pd.DataFrame) -> pd.DataFrame:
+        """
+                        Method Name: remove_unwanted_spaces
+                        Description: This method removes the unwanted spaces from a pandas dataframe.
+                        Output: A pandas DataFrame after removing the spaces.
+                        On Failure: Raise Exception
+
+                        Written By: iNeuron Intelligence
+                        Version: 1.0
+                        Revisions: None
+
+                """
+
+        try:
+            df_without_spaces = data.apply(
+                lambda x: x.str.strip() if x.dtype == "object" else x)  # drop the labels specified in the columns
+            logging.info(
+                'Unwanted spaces removal Successful.Exited the remove_unwanted_spaces method of the Preprocessor class')
+            return df_without_spaces
+        except Exception as e:
+            raise CustomException(e, sys)
+
+    @staticmethod
+    def identify_feature_types(dataframe: pd.DataFrame):
+        data_types = dataframe.dtypes
+
+        categorical_features = []
+        continuous_features = []
+        discrete_features = []
+
+        for column, dtype in dict(data_types).items():
+            unique_values = dataframe[column].nunique()
+
+            if dtype == 'object' or unique_values < 10:  # Consider features with less than 10 unique values as categorical
+                categorical_features.append(column)
+            elif dtype in [np.int64, np.float64]:  # Consider features with numeric data types as continuous or discrete
+                if unique_values > 20:  # Consider features with more than 20 unique values as continuous
+                    continuous_features.append(column)
+                else:
+                    discrete_features.append(column)
+            else:
+                # Handle other data types if needed
+                pass
+
+        return categorical_features, continuous_features, discrete_features
+        
+    
+
+
